7565ea56bee76333657703426079a451c359b6aa
Yiwan Zhou <mail@yiwan.me>
2025-03-10 04:33:11 +0000

    wip

--- packages/ui/src/components/breadcrumb.css.ts
+++ packages/ui/src/components/breadcrumb.css.ts
@@ -1,5 +1,4 @@
 import { style } from '@vanilla-extract/css'
-import { semanticVars } from '../theme.css'
 
 export const breadcrumbsClassName = style({
   display: 'flex',
@@ -16,37 +15,6 @@ export const breadcrumbClassName = style({
     [`${breadcrumbsClassName} &:not(:last-child)::after`]: {
       content: '›',
       padding: '0 5px'
-    }
-  }
-})
-
-export const breadcrumbLinkClassName = style({
-  color: semanticVars.color.linkColorSecondary,
-  outline: 'none',
-  position: 'relative',
-  textDecoration: 'none',
-  cursor: 'pointer',
-
-  selectors: {
-    '&[data-hovered]': {
-      textDecoration: 'underline'
-    },
-    '&[data-current]': {
-      color: 'var(--text-color)',
-      fontWeight: 'bold'
-    },
-    '&[data-focus-visible]::after': {
-      content: '',
-      position: 'absolute',
-      inset: '-2px -4px',
-      borderRadius: '6px',
-      border: `2px solid ${semanticVars.color.focusRingColor}`
-    },
-    '&[data-disabled]': {
-      cursor: 'default'
-    },
-    '&[data-disabled]:not([data-current])': {
-      color: semanticVars.color.textColorDisabled
     }
   }
 })
--- packages/ui/src/components/breadcrumbs.tsx
+++ packages/ui/src/components/breadcrumbs.tsx
@@ -1,7 +1,16 @@
-import { BreadcrumbsProps, Breadcrumbs as RACBreadcrumbs } from 'react-aria-components'
-import { breadcrumbsClassName } from './breadcrumb.css'
+import {
+  type BreadcrumbProps,
+  type BreadcrumbsProps,
+  Breadcrumb as RACBreadcrumb,
+  Breadcrumbs as RACBreadcrumbs
+} from 'react-aria-components'
+import { breadcrumbClassName, breadcrumbsClassName } from './breadcrumb.css'
 import { cn } from './utils'
 
 export function Breadcrumbs<T extends object>(props: BreadcrumbsProps<T>) {
   return <RACBreadcrumbs {...props} className={cn(props.className, breadcrumbsClassName)} />
+}
+
+export function Breadcrumb(props: BreadcrumbProps) {
+  return <RACBreadcrumb {...props} className={cn(props.className, breadcrumbClassName)} />
 }

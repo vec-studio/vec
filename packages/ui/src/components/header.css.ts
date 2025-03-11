bd77ffbbf78254845aee3e4ce2591271f231e756
Yiwan Zhou <mail@yiwan.me>
2025-03-11 12:24:23 +0000

    wip

--- packages/ui/src/components/header.css.ts
+++ packages/ui/src/components/header.css.ts
@@ -1,9 +1,17 @@
 import { style } from '@vanilla-extract/css'
+import { listBoxClassName } from './list-box.css'
 import { menuClassName } from './menu.css'
 
 export const headerClassName = style({
   selectors: {
+    // menu
     [`${menuClassName} &`]: {
+      fontSize: '1.143rem',
+      fontWeight: 'bold',
+      padding: '0 0.714rem'
+    },
+    // list box
+    [`${listBoxClassName} &`]: {
       fontSize: '1.143rem',
       fontWeight: 'bold',
       padding: '0 0.714rem'

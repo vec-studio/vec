ad8565c67b7cdefe8e66d6bfe7b1ddebc6a081b3
Yiwan Zhou <mail@yiwan.me>
2025-03-23 07:25:48 +0000

    wip

--- packages/ui/src/components/tag-group.css.ts
+++ packages/ui/src/components/tag-group.css.ts
@@ -1,3 +1,19 @@
-import { style } from '@vanilla-extract/css'
+import { globalStyle, style } from '@vanilla-extract/css'
+import { semanticVars } from '../theme.css'
 
-export const tagGroupClassName = style({})
+export const tagGroupClassName = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '2px',
+  fontSize: 'small',
+  color: semanticVars.color.textColor
+})
+
+globalStyle(`${tagGroupClassName} & [slot=description]`, {
+  fontSize: '12px'
+})
+
+globalStyle(`${tagGroupClassName} & [slot=errorMessage]`, {
+  fontSize: '12px',
+  color: semanticVars.color.invalidColor
+})
--- packages/ui/src/components/tag-list.css.ts
+++ packages/ui/src/components/tag-list.css.ts
@@ -1,3 +1,7 @@
 import { style } from '@vanilla-extract/css'
 
-export const tagListClassName = style({})
+export const tagListClassName = style({
+  display: 'flex',
+  flexWrap: 'wrap',
+  gap: '4px'
+})

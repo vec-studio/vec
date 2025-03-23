c2beef322bb0b1d45fb44cfc1fe9ca113ce390bb
Yiwan Zhou <mail@yiwan.me>
2025-03-23 12:44:07 +0000

    wip

--- packages/ui/.ladle/components.tsx
+++ packages/ui/.ladle/components.tsx
@@ -1,6 +1,10 @@
-import { type GlobalProvider } from '@ladle/react'
-import { semanticClassName, themeLightClassName } from '../src/theme.css'
+import { ThemeState, type GlobalProvider } from '@ladle/react'
+import { semanticClassName, themeDarkClassName, themeLightClassName } from '../src/theme.css'
 
 export const Provider: GlobalProvider = ({ children, globalState, storyMeta }) => (
-  <div className={`${themeLightClassName} ${semanticClassName}`}>{children}</div>
+  <div
+    className={`${globalState.theme === ThemeState.Light ? themeLightClassName : themeDarkClassName} ${semanticClassName}`}
+  >
+    {children}
+  </div>
 )

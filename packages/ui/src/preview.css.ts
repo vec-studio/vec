248b132751d792e82138d29458c6f16769a36c3c
Yiwan Zhou <mail@yiwan.me>
2025-03-23 14:43:46 +0000

    wip

--- packages/ui/src/preview.css.ts
+++ packages/ui/src/preview.css.ts
@@ -4,6 +4,19 @@ export const previewClassName = style({
   margin: 10,
   display: 'grid',
   gap: 10,
-  gridAutoRows: 'max-content',
-  gridAutoColumns: 'max-content'
+  gridAutoFlow: 'column',
+  gridTemplateRows: 'max-content',
+  gridTemplateColumns: 'max-content'
 })
+
+export const previewCardClassName = style({
+  padding: 30,
+  boxShadow: '0 0 10px #eee',
+  borderRadius: 5
+})
+
+export const previewCardTitleClassName = style({
+  marginBottom: 10
+})
+
+export const previewCardContentClassName = style({})
--- packages/ui/src/preview.tsx
+++ packages/ui/src/preview.tsx
@@ -1,19 +1,50 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Autocomplete, Button, MenuItem } from './components'
-import { previewClassName } from './preview.css'
+import { Autocomplete, Button, MenuItem, ToggleButton, ToggleButtonGroup } from './components'
+import {
+  previewCardClassName,
+  previewCardContentClassName,
+  previewCardTitleClassName,
+  previewClassName
+} from './preview.css'
 import { semanticClassName, themeLightClassName } from './theme.css'
 
 function Components() {
   return (
-    <div className={`${themeLightClassName} ${semanticClassName} ${previewClassName}`}>
-      <div>
-        <Autocomplete label="label" placeholder="placeholder">
-          <MenuItem>42</MenuItem>
-        </Autocomplete>
-      </div>
-      <div>
-        <Button>42</Button>
+    <div className={`${themeLightClassName} ${semanticClassName}`}>
+      <div className={`${previewClassName}`}>
+        <div className={previewCardClassName}>
+          <div className={previewCardTitleClassName}>Autocomplete</div>
+          <div className={previewCardContentClassName}>
+            <Autocomplete label="label" placeholder="placeholder">
+              <MenuItem>Option 1</MenuItem>
+              <MenuItem>Option 2</MenuItem>
+              <MenuItem>Option 3</MenuItem>
+            </Autocomplete>
+          </div>
+        </div>
+        <div className={previewCardClassName}>
+          <div className={previewCardTitleClassName}>Button</div>
+          <div className={previewCardContentClassName}>
+            <Button>Press me</Button>
+          </div>
+        </div>
+        <div className={previewCardClassName}>
+          <div className={previewCardTitleClassName}>Toggle Button</div>
+          <div className={previewCardContentClassName}>
+            <ToggleButton>Pin</ToggleButton>
+          </div>
+        </div>
+        <div className={previewCardClassName}>
+          <div className={previewCardTitleClassName}>Toggle Button Group</div>
+          <div className={previewCardContentClassName}>
+            <ToggleButtonGroup>
+              <ToggleButton id="left">Left</ToggleButton>
+              <ToggleButton id="center">Center</ToggleButton>
+              <ToggleButton id="right">Right</ToggleButton>
+            </ToggleButtonGroup>
+          </div>
+        </div>
       </div>
     </div>
   )

32104ebb696232c185918dc25b6b01a1ea84c53b
Yiwan Zhou <mail@yiwan.me>
2025-03-25 13:07:09 +0000

    wip

--- packages/ui/src/preview/card.css.ts
+++ packages/ui/src/preview/card.css.ts
@@ -1,14 +1,33 @@
-import { style } from '@vanilla-extract/css'
+import { globalStyle, style } from '@vanilla-extract/css'
+import { semanticVars, themeVars } from '../theme.css'
 
 export const cardClassName = style({
   borderRadius: '.5rem',
-  boxShadow: '0 0 .5rem #e0e0e0',
+  display: 'flex',
+  flexDirection: 'column',
+  minWidth: '15rem',
   padding: '1rem',
-  minWidth: '15rem'
+  backgroundColor: 'rgb(250, 250, 250)'
 })
 
 export const cardTitleClassName = style({
-  marginBottom: '.5rem'
+  fontWeight: '700',
+  lineHeight: '1.3',
+  marginBottom: '1rem'
 })
 
-export const cardContentClassName = style({})
+export const cardContentClassName = style({
+  borderRadius: '0.285rem',
+  flex: 1,
+  padding: '1rem'
+})
+
+globalStyle(`${cardClassName} hr`, {
+  display: 'none',
+  backgroundColor: themeVars.color.gray100,
+  blockSize: '0.285rem',
+  border: 'none',
+  borderRadius: '0.142rem',
+  marginBottom: '1rem',
+  width: '100%'
+})
--- packages/ui/src/preview/card.tsx
+++ packages/ui/src/preview/card.tsx
@@ -9,6 +9,7 @@ export const Card: FunctionComponent<{ title: string; children: ReactNode } & HT
   return (
     <div {...props} className={cardClassName}>
       <div className={cardTitleClassName}>{title}</div>
+      <hr />
       <div className={cardContentClassName}>{children}</div>
     </div>
   )

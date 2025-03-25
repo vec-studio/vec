1585775dfb10355a2c90a72ee17217714fa382c6
Yiwan Zhou <mail@yiwan.me>
2025-03-25 09:28:28 +0000

    wip

--- packages/ui/src/preview/card.css.ts
+++ packages/ui/src/preview/card.css.ts
@@ -2,7 +2,7 @@ import { style } from '@vanilla-extract/css'
 
 export const cardClassName = style({
   borderRadius: '.5rem',
-  boxShadow: '0 0 .5rem #eee',
+  boxShadow: '0 0 .5rem #e0e0e0',
   padding: '1rem',
   minWidth: '15rem'
 })

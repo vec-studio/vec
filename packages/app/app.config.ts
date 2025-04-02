7552b740eb049eece3141931ca5956c8a07ce06e
Yiwan Zhou <mail@yiwan.me>
2025-04-02 14:44:33 +0000

    wip

--- packages/app/app.config.ts
+++ packages/app/app.config.ts
@@ -3,20 +3,25 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
-  vite: {
-    resolve: {
-      alias: {
-        src: fileURLToPath(new URL('src', import.meta.url)),
-        public: fileURLToPath(new URL('public', import.meta.url))
-      }
-    },
-    plugins: [vanillaExtractPlugin()]
-  },
   tsr: {
     appDirectory: './src',
     autoCodeSplitting: true,
     routesDirectory: './src',
     target: 'react',
     virtualRouteConfig: './routes.ts'
+  },
+  vite: {
+    plugins: [vanillaExtractPlugin()],
+    resolve: {
+      alias: {
+        public: fileURLToPath(new URL('public', import.meta.url)),
+        src: fileURLToPath(new URL('src', import.meta.url))
+      }
+    },
+    server: {
+      hmr: {
+        clientPort: process.env.PORT
+      }
+    }
   }
 })

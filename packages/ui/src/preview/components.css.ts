6413132875e6c6e6e5266b9bf1b432aadf81a756
Yiwan Zhou <mail@yiwan.me>
2025-04-20 09:15:23 +0000

    chore

--- packages/ui/src/preview/components.css.ts
+++
@@ -1,7 +0,0 @@
-import { style } from '@vanilla-extract/css'
-
-export const componentsClassName = style({
-  display: 'flex',
-  flexWrap: 'wrap',
-  gap: '1rem'
-})
--- packages/ui/src/preview/index.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Root } from './root'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Root />
-  </StrictMode>
-)

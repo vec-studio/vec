2595a0d34369c697434c239ca8b4235421212cbe
Yiwan Zhou <mail@yiwan.me>
2025-04-23 05:28:02 +0000

    wip

--- packages/ui/src/components/link.tsx
+++
@@ -1,7 +0,0 @@
-import { Link as RACLink, type LinkProps } from 'react-aria-components'
-import { linkClassName } from './link.css'
-import { cn } from './utils'
-
-export function Link(props: LinkProps) {
-  return <RACLink {...props} className={cn(props.className, linkClassName)} />
-}

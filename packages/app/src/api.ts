2ed2e80c0873c0d1b6956e16e0583cefe7c2c5ee
Yiwan Zhou <mail@yiwan.me>
2025-05-10 17:01:46 +0000

    wip

--- packages/app/src/api.ts
+++
@@ -1,7 +0,0 @@
-import { createStartAPIHandler, defaultAPIRoutesHandler } from '@tanstack/react-start/api'
-
-export default createStartAPIHandler(
-  defaultAPIRoutesHandler({
-    routes: {}
-  })
-)

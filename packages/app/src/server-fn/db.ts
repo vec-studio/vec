7c19c426295dc5ffcf5e58292fe9011a4be575c1
Yiwan Zhou <mail@yiwan.me>
2025-07-20 15:56:53 +0000

    wip

--- packages/app/src/server-fn/db.ts
+++ packages/app/src/server-fn/db.ts
@@ -1,5 +1,1 @@
-import { DuckDBInstance } from '@duckdb/node-api'
 import { env } from 'std-env'
-
-const instance = await DuckDBInstance.create(env.DUCKDB_PATH)
-export const connection = await instance.connect()

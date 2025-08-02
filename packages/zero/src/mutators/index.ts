363f3f06e4082bcc3c26004966229fe095631c01
Yiwan Zhou <mail@yiwan.me>
2025-08-02 01:43:20 +0000

    wip

--- packages/zero/src/mutators/index.ts
+++
@@ -1,9 +0,0 @@
-import { CustomMutatorDefs } from '@rocicorp/zero'
-import { type AuthData } from '../permissions'
-import { type Schema } from '../schema'
-
-export function createMutators(authData: AuthData | undefined) {
-  return {} as const satisfies CustomMutatorDefs<Schema>
-}
-
-export type Mutators = ReturnType<typeof createMutators>
--- packages/zero/src/schema/index.ts
+++
@@ -1,9 +0,0 @@
-import { createSchema, json, string, table } from '@rocicorp/zero'
-
-const record = table('record').columns({ id: string(), value: json() }).primaryKey('id')
-
-export const schema = createSchema({
-  tables: [record]
-})
-
-export type Schema = typeof schema

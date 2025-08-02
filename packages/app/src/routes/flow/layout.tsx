0722faa695a56581fc2ccb39893b5bf0d2ccb68f
Yiwan Zhou <mail@yiwan.me>
2025-08-02 09:59:42 +0000

    wip

--- packages/app/src/routes/flow/layout.tsx
+++ packages/app/src/routes/flow/layout.tsx
@@ -1,7 +1,12 @@
+import { View } from '@adobe/react-spectrum'
 import { createFileRoute, Outlet } from '@tanstack/react-router'
 
 function component() {
-  return <Outlet />
+  return (
+    <View height="100vh">
+      <Outlet />
+    </View>
+  )
 }
 
 export const Route = createFileRoute('/_layout/flow/_layout')({

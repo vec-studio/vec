5fa22f6d73f1cfeec8cab56b0e463c46a7e53a40
Yiwan Zhou <mail@yiwan.me>
2025-08-02 13:00:22 +0000

    wip

--- packages/app/src/state/flow.ts
+++ packages/app/src/state/flow.ts
@@ -1,10 +1,10 @@
 import { createCollection, localStorageCollectionOptions } from '@tanstack/react-db'
 import z from 'zod'
 
-export const userPreferencesCollection = createCollection(
+export const userFlowCollection = createCollection(
   localStorageCollectionOptions({
     id: 'flow',
-    storageKey: 'vec-state-flow',
+    storageKey: 'vec-collection-flow',
     getKey: item => item.id,
     schema: z.object({})
   })

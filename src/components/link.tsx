8cb8966cfe2e285f65bb4b86b2e4ccb00ef759d1
Yiwan Zhou <mail@yiwan.me>
2025-08-09 06:50:10 +0000

    wip

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,6 +1,24 @@
-import { Button, Item, Link as SpectrumLink } from '@adobe/react-spectrum'
+import { Button, SpectrumButtonProps, Link as SpectrumLink, type SpectrumLinkProps } from '@adobe/react-spectrum'
 import { createLink } from '@tanstack/react-router'
+import { type HTMLAttributes } from 'react'
 
-export const Link = createLink(SpectrumLink)
-export const ItemLink = createLink(Item)
-export const ButtonLink = createLink(Button)
+interface CustomLinkProps extends SpectrumLinkProps {
+  className?: HTMLAttributes<unknown>['className']
+}
+
+function CustomLink(props: CustomLinkProps) {
+  const { className, ...rest } = props
+  return <SpectrumLink {...rest} UNSAFE_className={className} />
+}
+
+interface CustomButtonLinkProps extends SpectrumButtonProps {
+  className?: HTMLAttributes<unknown>['className']
+}
+
+function CustomButtonLink(props: CustomButtonLinkProps) {
+  const { className, ...rest } = props
+  return <Button {...rest} UNSAFE_className={className} />
+}
+
+export const Link = createLink(CustomLink)
+export const ButtonLink = createLink(CustomButtonLink)

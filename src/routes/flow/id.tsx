a5100613d8e3560e2d45676ca7d86d58924c827a
Yiwan Zhou <mail@yiwan.me>
2025-08-10 04:36:31 +0000

    wip

--- src/routes/flow/id.tsx
+++ src/routes/flow/id.tsx
@@ -2,7 +2,8 @@ import { Item, Menu } from '@adobe/react-spectrum'
 import { createFileRoute } from '@tanstack/react-router'
 import { ReactFlow } from '@xyflow/react'
 import '@xyflow/react/dist/style.css'
-import { type MouseEvent, useCallback, useRef, useState } from 'react'
+import { type MouseEventHandler, useRef, useState } from 'react'
+import { Popover } from 'react-aria-components'
 import * as hooks from 'src/hooks'
 import { useTranslations } from 'use-intl'
 
@@ -11,14 +12,21 @@ function component() {
   const t = useTranslations()
 
   const ref = useRef<HTMLDivElement>(null)
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-  const onPaneClick = useCallback(() => {
-    setIsMenuOpen(false)
-  }, [])
-  const onContextMenu = useCallback((e: MouseEvent) => {
+  const [menuPosition, setMenuPosition] = useState<{
+    offset: number
+    crossOffset: number
+  } | null>(null)
+  const onContextMenu: MouseEventHandler<HTMLDivElement> = e => {
     e.preventDefault()
-    setIsMenuOpen(true)
-  }, [])
+    const rect = e.currentTarget.getBoundingClientRect()
+    setMenuPosition({
+      offset: e.clientY - rect.bottom,
+      crossOffset: e.clientX - rect.left
+    })
+  }
+  const onPaneClick = () => setMenuPosition(null)
+  const onOpenChange = onPaneClick
+  const onClose = onPaneClick
 
   const { nodes, edges } = hooks.flow.useNodesEdges(params.id)
   const onNodesChange = hooks.flow.useOnNodesChange()
@@ -28,21 +36,31 @@ function component() {
   return (
     <>
       <ReactFlow
-        ref={ref}
-        nodes={nodes}
+        aria-haspopup="menu"
         edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        onPaneClick={onPaneClick}
-        onContextMenu={onContextMenu}
         fitView
+        nodes={nodes}
+        onConnect={onConnect}
+        onContextMenu={onContextMenu}
+        onEdgesChange={onEdgesChange}
+        onNodesChange={onNodesChange}
+        onPaneClick={onPaneClick}
+        ref={ref}
       />
-      {isMenuOpen && (
-        <Menu>
+      <Popover
+        crossOffset={menuPosition?.crossOffset}
+        isNonModal={true}
+        isOpen={!!menuPosition}
+        offset={menuPosition?.offset}
+        onOpenChange={onOpenChange}
+        placement="bottom start"
+        shouldFlip={false}
+        triggerRef={ref}
+      >
+        <Menu aria-label="menu" onClose={onClose}>
           <Item key="add">{t('flow.context-menu.add')}</Item>
         </Menu>
-      )}
+      </Popover>
     </>
   )
 }
